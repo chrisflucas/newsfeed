38d8124ba4a1e832cc4312aa4561041d19dd2512
Charles Lu <charleslu@live.com>
2015-05-30 17:05:36 -0700

    Define post schema

--- models/post.js
+++ models/post.js
@@ -1,3 +1,12 @@
 var mongoose = require('mongoose');
 
-// TODO
+var postSchema = mongoose.Schema({
+    api: String,
+    source: String,
+    title: String,
+    upvotes: Number
+});
+
+var Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
