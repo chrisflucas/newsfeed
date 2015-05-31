92fa307da7fcdbf064c1068cf90eb7c9173eb3a7
Charles Lu <charleslu@live.com>
2015-05-30 21:30:39 -0700

    Implement post API

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var soundcloud = require('../lib/soundcloud');
 var youtube = require('../lib/youtube');
 var flickr = require('../lib/flickr');
 
+var Post = require('../models/post');
+
 var NUM_APIS = 3;
+var STATUS_OK = 200;
 
 module.exports = function(app) {
   /* Renders the newsfeed landing page. */
@@ -21,7 +24,7 @@ module.exports = function(app) {
       allResults.push(results[0]);
 
       apisDone++;
-      if (apisDone === NUM_APIS) response.json(200, allResults);
+      if (apisDone === NUM_APIS) response.json(STATUS_OK, allResults);
     });
 
     youtube.search(query, function (error, results) {
@@ -30,7 +33,7 @@ module.exports = function(app) {
       allResults.push(results[0]);
 
       apisDone++;
-      if (apisDone === NUM_APIS) response.json(200, allResults);
+      if (apisDone === NUM_APIS) response.json(STATUS_OK, allResults);
     });
 
     flickr.search(query, function (error, results) {
@@ -39,7 +42,50 @@ module.exports = function(app) {
       allResults.push(results[0]);
 
       apisDone++;
-      if (apisDone === NUM_APIS) response.json(200, allResults);
+      if (apisDone === NUM_APIS) response.json(STATUS_OK, allResults);
+    });
+  });
+
+  // Read posts
+  app.get('/posts', function (request, response) {
+    Posts.find(function (error, posts) {
+      if (error) throw error;
+      response.json(STATUS_OK, people);
+    });
+  });
+
+  // Create new post
+  app.post('/posts', function (request, response) {
+    var post = new Post({
+      api: request.body.api,
+      source: request.body.source,
+      title: request.body.title,
+      upvotes: 0
+    });
+
+    post.save(function (error) {
+      if (error) throw error;
+      response.json(STATUS_OK, post);
+    });
+  });
+
+  // Delete a post
+  app.post('/posts/remove', function (request, response) {
+    Post.findByIdAndRemove(request.body.id, function (error) {
+      if (error) throw error;
+      response.send(STATUS_OK);
+    });
+  });
+
+  // Upvote a post
+  app.post('/posts/upvote', function (request, response) {
+    Post.findById(request.body.id, function (error, post) {
+      if (error) throw error;
+
+      post.upvotes++;
+      post.save(function (error) {
+        if (error) throw error;
+      });
     });
   });
 };
